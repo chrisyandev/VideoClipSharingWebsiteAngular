503ffff7620fd5c9bb8b70c95f31177b0a8fc32d
chrisyandev <czyanhome@hotmail.com>
2022-04-28 15:25:43 -0700

    Fixing the memory leak

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -21,6 +21,12 @@ export class ModalService {
     console.log(this.modals)
   }
 
+  unregister(id: string) {
+    this.modals = this.modals.filter(
+      element => element.id !== id
+    )
+  }
+
   isModalOpen(id: string) : boolean {
     return Boolean(this.modals.find(element => element.id === id)?.visible)
   }
